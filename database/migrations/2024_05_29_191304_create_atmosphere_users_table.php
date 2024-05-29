4fb6321c75f9337e12f66d297de8e47c8460dc3c
allen <soramagadia@gmail.com>
2024-05-30 03:19:16 +0800

    added migration tables

--- database/migrations/2024_05_29_191304_create_atmosphere_users_table.php
+++ database/migrations/2024_05_29_191304_create_atmosphere_users_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('atmosphere_users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('private_key');
-            $table->rememberToken();
+            $table->foreignId('atmosphere_id')->constrained('atmospheres')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamp('joined_at')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('atmosphere_users');
     }
 };
