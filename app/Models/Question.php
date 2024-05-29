332df60a98c027d41a01a16d319e276fc8561259
allen <soramagadia@gmail.com>
2024-05-30 03:50:18 +0800

    added table models

---
+++ app/Models/Question.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['content'];
+
+    public function atmospheres() {  
+        return $this->belongsToMany(Atmosphere::class, 'atmosphere_questions')
+        ->withTimestamps();
+    }
+
+    public function answers() {
+        return $this->hasMany(Answer::class);
+    }
+}
